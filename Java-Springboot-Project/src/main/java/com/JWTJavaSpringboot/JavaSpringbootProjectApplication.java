cc93b8411ebc57a70a239f555498d2637eed329b
SebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-09-29 20:35:56 -0300

    initial commit

---
+++ Java-Springboot-Project/src/main/java/com/JWTJavaSpringboot/JavaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.JWTJavaSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootProjectApplication.class, args);
+	}
+
+}
